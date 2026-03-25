d4a8f76ff8a7459799a7e354fb5e33aa30d6017c
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2026-03-25 23:17:43 +0800

    1.5.1 fix horse crash

--- src/main/java/com/vomiter/zombieseatanimals/event/HorseDeathEventHandler.java
+++ src/main/java/com/vomiter/zombieseatanimals/event/HorseDeathEventHandler.java
@@ -50,7 +50,6 @@ public class HorseDeathEventHandler {
                                 (ServerLevelAccessor) level,
                                 level.getCurrentDifficultyAt(zombieHorse.blockPosition()),
                                 MobSpawnType.CONVERSION,
-                                null,
                                 null
                         );
 
