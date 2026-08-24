42a599a5b27bc06c40bf9cf53460371c793dcac0
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2026-08-24 19:04:38 +0800

    1.6.2 issue #8, #9
    (cherry picked from commit f60b495bd14bd23d957f6c90f0ff5668fa09357b)

--- src/main/java/com/vomiter/zombieseatanimals/mixin/LivingEntityAccessor.java
+++ src/main/java/com/vomiter/zombieseatanimals/mixin/LivingEntityAccessor.java
@@ -1,6 +1,5 @@
 package com.vomiter.zombieseatanimals.mixin;
 
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
@@ -8,7 +7,7 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(LivingEntity.class)
 public interface LivingEntityAccessor {
     @Invoker("dropAllDeathLoot")
-    void dropAllDeathLoot(
+    void zea$dropAllDeathLoot(
             net.minecraft.server.level.ServerLevel p_level,
             net.minecraft.world.damagesource.DamageSource damageSource
     );
